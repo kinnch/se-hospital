4d8d247800a31e59285b7b4c5c63cca467364650
Kinnch <kinnch@gmail.com>
2016-10-22 12:00:28 +0700

    return index.html for all incoming backend request

--- routes/route.js
+++ routes/route.js
@@ -6,12 +6,12 @@ module.exports = function(app) {
     patientController.setDBConnectionsFromApp(app);
 
     function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
+		if(req.isAuthenticated()){
+			return next();
+		} else {
+			//req.flash('error_msg','You are not logged in');
+			res.redirect('/users/login');
+		}
 	}
-
 }
 
