9201e53deef561bb708d04a97505058570cd6c5f
Kinnch <kinnch@gmail.com>
2016-10-22 14:47:15 +0700

    calendar doctor

---
+++ src/app/DoctorCalendarComponent/doctor-calendar.component.ts
@@ -0,0 +1,37 @@
+import {ScheduleModule} from 'primeng/primeng';
+import {Component , OnInit } from "@angular/core";
+
+@Component({
+    selector : `doctor-calendar`,
+    template : `<p-schedule [events]="events"></p-schedule>`
+})
+export class DoctorCalendarComponent implements OnInit {
+    events: any[];
+
+    ngOnInit() {
+        this.events = [
+            {
+                "title": "All Day Event",
+                "start": "2016-01-01"
+            },
+            {
+                "title": "Long Event",
+                "start": "2016-01-07",
+                "end": "2016-01-10"
+            },
+            {
+                "title": "Repeating Event",
+                "start": "2016-01-09T16:00:00"
+            },
+            {
+                "title": "Repeating Event",
+                "start": "2016-01-16T16:00:00"
+            },
+            {
+                "title": "Conference",
+                "start": "2016-01-11",
+                "end": "2016-01-13"
+            }
+        ];
+    }
+}
