8eb9d42d42aae8467d9f5dbee68063d3ac69e6dd
Kinnch <kinnch@gmail.com>
2016-10-22 13:40:15 +0700

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     filename: "./dist/[name].bundle.js"
   },
   resolve: {
-    extensions: ['', '.js','html','css']
+    extensions: ['', '.js','html','css','jpeg','jpg','png','gif','svg']
   },
   devtool: 'eval-source-map',
   module: {
@@ -21,6 +21,13 @@ module.exports = {
         test: /\.css/,
         loaders: ['to-string-loader','css-loader']
         // exclude: /node_modules/
+      },
+       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       },
       { test: /\.html$/, loader: 'html-loader?minimise=true' },
       { test: /\.json$/, loader: 'json-loader' }
