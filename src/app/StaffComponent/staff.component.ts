5981d00373648840f89fe8abf9697580b8ccae4a
Pohfy123 <pohfy123@gmail.com>
2016-10-22 18:23:29 +0700

    fix bug active class

--- src/app/StaffComponent/staff.component.ts
+++ src/app/StaffComponent/staff.component.ts
@@ -7,25 +7,32 @@ import { Router } from '@angular/router';
 })
 
 export class StaffComponent {
-    constructor(private router: Router) {}
-    isExpanded: Boolean = true;
+    constructor(private router: Router) {
+        if(this.router.url === '/manage/manage_queue'){
+            this.activatedClass = 4;
+        }
+    }
+    // isExpanded: Boolean = true;
     activatedClass = 1; //1 จัดการผู้ป่วย ,2,3,4
+
     gotoPage(menu):void{
         let link = ['manage'];
         this.activatedClass = menu;
-        if(menu === 1){
+        if(menu === 1){ // 1 จัดการผู้ป่วย
             link = ['manage'];
         } else if(menu === 2){
             link = ['manage'];
         } else if(menu === 3){
             link = ['manage'];
-        } else if(menu === 4){
+        } else if(menu === 4){ // 4 Checkin เข้าห้อง
             link = ['manage','manage_queue'];
+        } else if(menu===5){
+            link = ['manage','patient','hn']
         }
         this.router.navigate(link);
     }
-    navToggle(){
-        console.log("pohfy"); 
-    }
+    // navToggle(){
+    //     console.log("pohfy"); 
+    // }
 }
 
