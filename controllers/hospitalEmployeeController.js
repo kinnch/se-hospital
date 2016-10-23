24bf817ceaf21216d6eb7bbd190c1907ee691c51
Keerato <Keerati.tan@gmail.com>
2016-10-23 11:51:26 +0700

    edit seeder + check employee api

--- controllers/hospitalEmployeeController.js
+++ controllers/hospitalEmployeeController.js
@@ -10,7 +10,7 @@ var HospitalEmployee = require("../model/hospitalEmployee");
 
 exports.isInSystem = function(req, res) {
     HospitalEmployee.findOne({
-        userName: req.body.username
+        userName: (req.body.username)+''
     }, function(err, employee){
         if(err) return console.error(err);
         if(employee == null) res.send(false);
