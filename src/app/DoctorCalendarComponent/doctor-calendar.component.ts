ea938920062a3a4736d6b4f503295282ef8c629e
Kinnch <kinnch@gmail.com>
2016-11-14 16:11:58 +0700

    update calendar

--- src/app/DoctorCalendarComponent/doctor-calendar.component.ts
+++ src/app/DoctorCalendarComponent/doctor-calendar.component.ts
@@ -27,19 +27,22 @@ export class DoctorCalendarComponent implements AfterViewInit, OnInit {
             //    (e["_id"]["period"] == "am")
                 this.events.push(
                     {
-                        title  : `<i class="fa fa-circle left-event" aria-hidden="true"><span class="text-primary">      ${e.doctors}/5</span></i> <i class="fa fa-circle right-event" aria-hidden="true">      ${e.patients}/15</i>`,
+                        title  : `<i class="fa fa-circle-o left-event" aria-hidden="true"><span class="text-primary">      ${e.doctors}/5</span></i> <i class="fa fa-circle-o right-event" aria-hidden="true">      ${e.patients}/15</i>`,
                         start  : moment(e["_id"]["date"]).format(),
                         color : e["_id"]["period"] == "am"? "rgba(98, 208, 233, 0.3)": "rgba(255, 255, 0, 0.3)",
-                        allDay: true
+                        allDay: true,
+                        id : "id_tmp" 
                     }
                 );
                
            })
         }).then(() => {
+            var self = this;
             jQuery('#calendar').fullCalendar({
                 aspectRatio: 1.35 ,
                 eventClick: function(calEvent, jsEvent, view) {
                     console.log('Event: ' + calEvent.title);
+                    console.log('Event id : '+ calEvent.id);
                     console.log('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
                     console.log('View: ' + view.name);
                 },
@@ -51,18 +54,25 @@ export class DoctorCalendarComponent implements AfterViewInit, OnInit {
             });
             $(window).on('resize', function () {
                 console.log("resize")
-                $('.fc-content').height( $('.fc-content').width()*0.350);
-                $('.left-event').css('margin-top',$('.fc-content').height()*0.5);
-                $('.left-event').css('margin-left',$('.fc-content').width()*0.2);
-                $('.right-event').css('margin-top',$('.fc-content').height()*0.5);
-                $('.right-event').css('margin-right',$('.fc-content').width()*0.2);
+                self.resizeData()
+                $(".fc-prev-button").on("click", function(){ self.resizeData() });
+                $(".fc-next-button").on("click", function(){ self.resizeData() });
             }).resize();
+
         });
         
     }
 
     callback(){
         alert("callback from modal");
+    }
+
+    resizeData(){
+        $('.fc-content').height( $('.fc-content').width()*0.350);
+        $('.left-event').css('margin-top',$('.fc-content').height()*0.5);
+        $('.left-event').css('margin-left',$('.fc-content').width()*0.2);
+        $('.right-event').css('margin-top',$('.fc-content').height()*0.5);
+        $('.right-event').css('margin-right',$('.fc-content').width()*0.2);
     }
     
 }
