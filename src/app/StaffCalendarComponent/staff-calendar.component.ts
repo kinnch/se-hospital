fa3204ba5e17cb38bf56485c642ceccddad8a38a
Kinnch <kinnch@gmail.com>
2016-11-18 04:17:57 +0700

    staff calendar and search dropdown select

--- src/app/StaffCalendarComponent/staff-calendar.component.ts
+++ src/app/StaffCalendarComponent/staff-calendar.component.ts
@@ -17,16 +17,28 @@ export class StaffCalendarComponent implements AfterViewInit, OnInit {
     numbers = Array(5).map((x,i)=>i);
     isOperate: boolean  = true;
     selectedEvent;
+    isFirst : boolean = true;
     issues=[
         {
             "key":1,
             "value" : 2,
-            "text" : "asdadasdas"
+            "text" : "นาย แพทย์ แปดสาวน้อย"
         },
         {
             "key":2,
             "value" : 3,
-            "text" : "asd"
+            "text" : "นาง แพทย์ แปดสาวน้อย2"
+        },
+        {
+            "key":3,
+            "value" : 3,
+            "text" : "นาง แพทย์ แปดสาวน้อย2"
+        },
+        
+        {
+            "key":4,
+            "value" : 3,
+            "text" : "นาง แพทย์ แปดสาวน้อย2"
         }
         ];
 
@@ -45,12 +57,15 @@ export class StaffCalendarComponent implements AfterViewInit, OnInit {
     fetchAndAdaptData(){
         this.elementService.getDoctorDateElements().then((jsonObject) => {
            jsonObject.table.forEach((e)=>{
-            //    (e["_id"]["period"] == "am")
+            var pic =  e["_id"]["period"] == "am" ? `<img src="/resources/images/sun-rise.png" height="85%" style="float: left;" >` : `<img src="/resources/images/noon.png" height="85%" style="float: left;" >`;
                 this.events.push(
                     {
-                        title  : `<i class="fa fa-circle-o left-event" aria-hidden="true"><span class="text-primary">      ${e.doctors}/5</span></i> `,
+                        title  : `${pic}<i class="fa fa-user-md left-event" aria-hidden="true"><span >      ${e.doctors}/5</span></i>
+                        <progress class="progress progress-info custom-progress" style=" margin-top: 8px;" value="50" max="100"></progress>
+                        
+                         `,
                         start  : moment(e["_id"]["date"]).format(),
-                        color : e["_id"]["period"] == "am"? "rgba(98, 208, 233, 0.3)": "rgba(255, 255, 0, 0.3)",
+                        color : e["_id"]["period"] == "am"? "rgba(171,71,188,1)": "rgb(88, 171, 39)",
                         allDay: true,
                         // url : e,
                         id : e 
@@ -85,11 +100,17 @@ export class StaffCalendarComponent implements AfterViewInit, OnInit {
             });
             $(window).on('resize', function () {
                 console.log("resize")
-                self.resizeData()
-                $(".fc-prev-button").on("click", function(){ self.resizeData() });
-                $(".fc-next-button").on("click", function(){ self.resizeData() });
+                // self.resizeData()
+                if(self.isFirst){
+                    self.isFirst = false;
+                    $(".fc-prev-button").on("click", function(){ $(window).resize(); self.resizeData() });
+                    $(".fc-next-button").on("click", function(){ $(window).resize(); self.resizeData() });       
+                }
+                
             }).resize();
-
+             self.resizeData()
+            $(window).resize();
+           
         });
         
     }
@@ -100,7 +121,9 @@ export class StaffCalendarComponent implements AfterViewInit, OnInit {
 
     resizeData(){
         $('.fc-content').height( $('.fc-content').width()*0.350);
-        $('.left-event').css('margin-top',$('.fc-content').height()*0.5);
+        $('.left-event').css('margin-top',$('.fc-content').height()*0.4);
+        $('progress').css('margin-top',$('.fc-content').height()*0.3);
+        
     }
     
 }
