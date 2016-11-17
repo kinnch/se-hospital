6c169c311a2f0d0b37405d62c9c6ca63496c384d
ikamm <yanisaaaaa.p@gmail.com>
2016-11-18 01:30:01 +0700

    link api with ui - all prescription for phar

--- src/app/PrescriptionListElementComponent/prescription-list-element.component.ts
+++ src/app/PrescriptionListElementComponent/prescription-list-element.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Router } from '@angular/router';
 import { PrescriptionListElement } from '../../models/prescription-list-element';
 import { PrescriptionTableComponent } from '../PrescriptionTableComponent/prescription-table.component';
+import * as moment_ from 'moment';
 @Component({
     selector: 'prescription-list-element-c',
     template: require('./prescription-list-element.component.html'),
@@ -10,9 +11,12 @@ import { PrescriptionTableComponent } from '../PrescriptionTableComponent/prescr
 })
 
 export class PrescriptionListElementComponent{
-    @Input() data : PrescriptionListElement ; //(0)หมอต้องแก้ Doctor needs to edit (1)หมอจ่ายมา Doctor prescipe, (2)เภสัชจ่ายแล้ว 
+    @Input() data ; //(0)หมอต้องแก้ Doctor needs to edit (1)หมอจ่ายมา Doctor prescipe, (2)เภสัชจ่ายแล้ว 
+    year: number;
+    month: number;
     constructor(private router: Router) {
     }
+
     gotoPage(hn):void{      
         this.router.navigate(['manage','prescription_request',hn]);
     }
@@ -20,4 +24,12 @@ export class PrescriptionListElementComponent{
     edit(obj):void{
         this.router.navigate(['manage','edit_prescription', obj]);
     }
+
+     ngOnInit(): void {
+        var diffDuration = moment_.duration(moment_().diff(this.data.patient.birthDate));
+        this.year = diffDuration.years();
+        this.month = diffDuration.months();
+       
+     }
+
 }
