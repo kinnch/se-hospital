0009ad4ec602b1784e6885b04c05965956160f71
Keerato <Keerati.tan@gmail.com>
2016-11-17 09:17:59 +0700

    add constrain

--- model/appointment.js
+++ model/appointment.js
@@ -5,10 +5,12 @@ var appointmentSchema = mongoose.Schema({
     patient: {
         type: Schema.Types.ObjectId, 
         ref: 'Patient',
+        required: true
     },
     status: {
         type: Number,
-        enum: [0,1,2,3,4]
+        enum: [0,1,2,3,4],
+        required: true
     },
     reason: String    
 });
