0e1d500a825e0c32d75b7758d902ef805359ed64
ikamm <yanisaaaaa.p@gmail.com>
2016-11-19 01:57:32 +0700

    link presChangeRequest with api

--- src/app/EditPrescriptionRequestComponent/edit-prescription-request.component.ts
+++ src/app/EditPrescriptionRequestComponent/edit-prescription-request.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Router } from '@angular/router';
 import { PrescriptionService } from '../../services/prescription.service';
-import { PrescriptionListElement } from '../../models/prescription-list-element';
+//import { PrescriptionListElement } from '../../models/prescription-list-element';
+
 @Component({
     selector: 'edit-prescription-request-c',
     template: require('./edit-prescription-request.component.html'),
@@ -11,11 +12,17 @@ import { PrescriptionListElement } from '../../models/prescription-list-element'
 
 export class EditPrescriptionRequestComponent{    
     role: string = 'doctor';
-    data: PrescriptionListElement[]; 
+    data: any;
+    doctorID: string = '582f2c2b56e1af4108ec3639';
+
     constructor(private router: Router, private prescriptionService: PrescriptionService) {
-        this.data=prescriptionService.getPrescriptionElements();
-        //this.prescriptionService.getPrescriptionElements().then(data => this.data = data);
-        console.log(this.data);
+    }
+
+    ngOnInit() {
+         this.prescriptionService.getPrescriptionChangeRequest(this.doctorID)
+        .then((rejectedData)=>{
+            this.data = rejectedData;
+        });
     }
 
     gotoPage(hn):void{      
