5ca40852213038dcf4909394492a000dd4ffe5f9
Napon Mekavuthikul <napon.meka@gmail.com>
2016-11-19 17:14:42 +0700

    doctor diagnosis conflict with nurse physical check fix

--- src/app/DiagnosisHistoryComponent/diagnosis-history.component.ts
+++ src/app/DiagnosisHistoryComponent/diagnosis-history.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
     selector: 'diagnosis-history-c',
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 })
 
 export class DiagnosisHistoryComponent {
+    @Input() allDiagnosisHistory:JSON;
     constructor(private router: Router) {
     }
 
