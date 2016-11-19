700da8a3181b35d504af1b8661a19e2526aabd7f
Napon Mekavuthikul <napon.meka@gmail.com>
2016-11-19 12:42:40 +0700

    patientDetailComponet info zone done

--- src/app/PatientDetailComponent/patient-detail.component.ts
+++ src/app/PatientDetailComponent/patient-detail.component.ts
@@ -1,14 +1,34 @@
-import {Component} from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params ,Router } from '@angular/router';
+import { AppointmentService } from '../../services/appointment.service';
+import {Subscription } from 'rxjs';
 @Component({
     selector: 'patient-detail-c',
     template: require('./patient-detail.component.html'),
     styles: [require('./patient-detail.component.css')]
 })
 
-export class PatientDetailComponent {
-    constructor(private router: Router) {}
+export class PatientDetailComponent implements OnInit{
+    HN: string;
+    private subscription: Subscription;
+    data: JSON;
+    found: boolean;
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, private appointmentService: AppointmentService) {}
     goback(): void{
         window.history.back();
+    }
+    ngOnInit(){
+        this.subscription = this.activatedRoute.params.subscribe(
+            (param: any) => {
+                this.HN = param['hn'];
+                console.log(this.HN);
+                this.appointmentService.getPatientAndAppointment(this.HN)
+                .then((data) => {
+                    console.log(data);
+                    this.data = data;
+                    this.found = true;
+                });
+        });
+        
     }
 }
