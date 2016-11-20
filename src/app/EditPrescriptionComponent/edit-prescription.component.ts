08eef28d255b7b527430fa49e1aff5fbb0066770
ikamm <yanisaaaaa.p@gmail.com>
2016-11-20 10:24:51 +0700

    prescriptionMakeChange link with api

--- src/app/EditPrescriptionComponent/edit-prescription.component.ts
+++ src/app/EditPrescriptionComponent/edit-prescription.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Router } from '@angular/router';
 import { PrescriptionFormComponent } from '../PrescriptionFormComponent/prescription-form.component'
 import {Subscription } from 'rxjs';
 import {OnInit, OnDestroy} from '@angular/core';
 import { PrescriptionService } from '../../services/prescription.service';
+import { ToastComponent } from '../ToastComponent/toast.component';
+
 
 @Component({
     selector: 'edit-prescription-c',
@@ -18,6 +20,8 @@ export class EditPrescriptionComponent{
     private subscription: Subscription;
     prescriptionToBeSave = [];
     newPres: string;
+        @ViewChild( ToastComponent ) toast: ToastComponent;
+
     constructor(private router: Router,private prescriptionService: PrescriptionService, private activatedRoute: ActivatedRoute) {
     }
 
@@ -54,5 +58,17 @@ export class EditPrescriptionComponent{
      confirm(){
          console.log('----Confirm----');
          console.log(this.prescriptionToBeSave);
+         this.prescriptionService.prescriptionChange(this.presID, this.prescriptionToBeSave)
+         .then((res) => {
+               if (res.status == "success") {
+                    this.toast.titleSuccess="ยืนยันการจ่ายยาสำเร็จ";
+                    this.toast.messageSuccess="";
+                    this.toast.addToastSuccess();
+                    this.router.navigate(['manage','edit_prescription_request']);
+
+                } else{
+                    this.toast.addToastError();
+                }
+            });
      }
 }
