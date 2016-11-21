6aa48a4ec5851b8b51f1c0b3eba35e993cac4307
ikamm <yanisaaaaa.p@gmail.com>
2016-11-21 20:01:46 +0700

    patient register ui & validate (not working)

--- src/app/RegisterComponent/register.component.ts
+++ src/app/RegisterComponent/register.component.ts
@@ -23,14 +23,24 @@ export class RegisterComponent implements OnInit{
     postCode: string;
     tel: string;
     email: string;
-    sex: string;
-    birthDate: string;
-    bloodType: string;
+    sex: string = "no";
+    birthDate: Date;
+    bloodType: string = "no";
 
     drugs: any; 
     currentDrugId: number = 1;
     allegicDrugsList : any[] = [];
     allegicDrugs: string[] = [];
+
+    errTitle = "";
+    errfirstName ="";
+    errlastName ="";
+    errnationalID = "";
+    errtel ="";
+    erremail ="";
+    errsex ="";
+    errbirthDate = "";
+    errbloodtype="";
 
     constructor(private router: Router, 
                 private location: Location,
@@ -66,18 +76,66 @@ export class RegisterComponent implements OnInit{
         this.location.back();
     }
     register(): void {
-        for(let drug of this.allegicDrugsList) {
-            if(drug['drugID'])
-                this.allegicDrugs.push(drug['drugID']);
-        }
-        this.patientService.createPatient(this.email, this.title, this.firstName, this.lastName, this.sex, this.birthDate, this.tel, this.nationalID, this.address, this.subdistrict, this.district, this.province, this.postCode, this.allegicDrugs, this.bloodType)
-        .then((res)=>{
-            console.log(res);
-            if(res.status == "success") {
-                console.log("register success!")
-            } else {
-                console.log("register fail!")
+       // if(this.validate()){
+            for(let drug of this.allegicDrugsList) {
+                if(drug['drugID'])
+                    this.allegicDrugs.push(drug['drugID']);
             }
-        });
+            this.patientService.createPatient(this.email, this.title, this.firstName, this.lastName, this.sex, this.birthDate, this.tel, this.nationalID, this.address, this.subdistrict, this.district, this.province, this.postCode, this.allegicDrugs, this.bloodType)
+            .then((res)=>{
+                console.log(res);
+                if(res.status == "success") {
+                    console.log("register success!")
+                } else {
+                    console.log("register fail!")
+                }
+            });
+        //}      
+    }
+
+    validate():boolean{
+        let isPass = false;
+        if(this.title == "") this.errTitle = "กรุณากรอกคำนำหน้าชื่อ";
+        else if(!/^[ก-๙|a-z|A-Z|\.]+$/.test(this.title)) this.errTitle = "คำนำหน้าชื่อควรประกอบด้วยอักขระภาษาไทย, อักขระภาษาอังกฤษ หรือจุด(.) และมีความยาวอย่างน้อย 2 ตัวอักษร"
+        else this.errTitle = "";
+
+        if(this.firstName == "") this.errfirstName = "กรุณากรอกชื่อ";
+        else if(!/^[ก-๙|a-z|A-Z]+$/.test(this.firstName)) this.errfirstName = "ชื่อควรประกอบด้วยอักขระภาษาไทย หรือภาษาอังกฤษเท่านั้น และมีความยาวไม่เกิน 50 ตัวอักษร";
+        else this.errfirstName = "";
+
+        if(this.lastName == "") this.errlastName = "กรุณากรอกนามสกุล";
+        else if(!/^[ก-๙|a-z|A-Z]+$/.test(this.lastName)) this.errlastName = "นามสกุลควรประกอบด้วยอักขระภาษาไทย หรือภาษาอังกฤษเท่านั้น และมีความยาวไม่เกิน 50 ตัวอักษร";
+        else this.errlastName = "";
+
+        if(this.nationalID != ""){
+            if(!/^\d{13}$/.test(this.nationalID)){
+                this.errnationalID = "รหัสประจำตัวประชาชนควรประกอบด้วยตัวเลข 0-9 และมีความยาว 13 ตัวอักษร";
+            }
+        }
+        else this.errnationalID = "";
+
+        if(this.tel == "") this.errtel = "กรุณากรอกเบอร์โทรศัพท์";
+        else if(!/^d{10}$/.test(this.tel)) this.errtel = "เบอร์โทรศัพท์ควรประกอบด้วยตัวเลข 0-9 และมีความยาว 10 ตัวอักษร"
+        else this.errtel = "";
+
+        if(this.email != ""){
+            if(!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/) this.erremail = "รูปแบบของ Email ไม่ถูกต้อง"
+        }
+        else this.erremail = "";
+
+        if(this.sex  == "no") this.errsex = "กรุณาเลือกเพศ";
+        else this.errsex = "";
+
+        if(this.bloodType == "no") this.errbloodtype = "กรุณาเลือกกรุ๊ปเลือด";
+        else this.errbloodtype = "";
+
+        if(this.birthDate == null) this.errbirthDate = "กรุณาเลือกวันเกิด";
+        else this.errbirthDate = "";
+
+        if(!this.errbirthDate && !this.errbloodtype && !this.erremail && !this.errfirstName && !this.errlastName && !this.errnationalID && !this.errsex && !this.errtel && !this.errTitle)
+            isPass = true;
+        console.log('-----');
+        console.log(isPass);
+        return isPass;
     }
 }
