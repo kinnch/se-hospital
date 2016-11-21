73e19f55cb18e91241cf8f268d7ad66081fc17a6
Keerato <Keerati.tan@gmail.com>
2016-11-21 16:34:38 +0700

    protect re HN for patient

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -229,6 +229,7 @@ exports.createHN = function(req, res){
         Patient.findOne({_id: req.body.patientID},function(err, patient){
             if(err) return res.send({status: 'fail'});
             if(!patient) return res.send({status: 'fail'});
+            if(patient.HN != null) return res.send({status: 'fail', msg: 'already have HN'});
             var newHN = (parseInt(maxHN) + 1) + '';
             while(newHN.length < 8){
                 newHN = '0' + newHN;
